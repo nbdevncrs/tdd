3767311f720f5df33d3d7b79413982f235f245f6
Matvey <rusinovmatvei@gmail.com>
2025-12-02 21:43:17 +0500

    edited CloudVisualizer.cs after code review

--- cs/TagsCloudCore/CloudVisualization/CloudVisualizer.cs
+++ cs/TagsCloudCore/CloudVisualization/CloudVisualizer.cs
@@ -1,20 +1,32 @@
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace TagsCloudCore.CloudVisualization
 {
     public static class CloudVisualizer
     {
-        public static void SaveLayoutToFile(string filePath, Rectangle[] rectangles, int padding = 50)
+        public static void SaveLayoutToFile(string filePath, IEnumerable<Rectangle> rectangles, int padding = 50)
         {
-            if (rectangles.Length == 0)
-                throw new ArgumentException("No rectangles to visualize");
+            var currentRectangles = rectangles.ToArray();
 
-            var minX = int.MaxValue;
-            var maxX = int.MinValue;
-            var minY = int.MaxValue;
-            var maxY = int.MinValue;
+            if (currentRectangles.Length == 0)
+            {
+                using var emptyBitmap = new Bitmap(300, 300);
+                using var g = Graphics.FromImage(emptyBitmap);
 
-            foreach (var r in rectangles)
+                g.Clear(Color.Black);
+                emptyBitmap.Save(filePath, ImageFormat.Png);
+                return;
+            }
+
+            var firstRectangle = currentRectangles[0];
+
+            var minX = firstRectangle.Left;
+            var maxX = firstRectangle.Right;
+            var minY = firstRectangle.Top;
+            var maxY = firstRectangle.Bottom;
+
+            foreach (var r in currentRectangles)
             {
                 if (r.Left < minX) minX = r.Left;
                 if (r.Right > maxX) maxX = r.Right;
@@ -25,34 +37,37 @@ namespace TagsCloudCore.CloudVisualization
             var width = (maxX - minX) + padding * 2;
             var height = (maxY - minY) + padding * 2;
 
-            var bitmap = new Bitmap(width, height);
-            var graphics = Graphics.FromImage(bitmap);
+            width = Math.Min(width, 5000);
+            height = Math.Min(height, 5000);
+
+            using var bitmap = new Bitmap(width, height);
+            using var graphics = Graphics.FromImage(bitmap);
 
             graphics.Clear(Color.Black);
 
+            using var pen = new Pen(Color.White, 1);
+            using var brush = new SolidBrush(Color.White);
+
             var random = new Random();
 
-            foreach (var rectangle in rectangles)
+            foreach (var rectangle in currentRectangles)
             {
                 var shifted = rectangle with
                 {
                     X = rectangle.Left - minX + padding, Y = rectangle.Top - minY + padding
                 };
 
-                var color = Color.FromArgb(
+                brush.Color = Color.FromArgb(
                     255,
                     random.Next(40, 255),
                     random.Next(40, 255),
                     random.Next(40, 255));
 
-                var brush = new SolidBrush(color);
-                var pen = new Pen(Color.White, 1);
-
                 graphics.FillRectangle(brush, shifted);
                 graphics.DrawRectangle(pen, shifted);
             }
 
-            bitmap.Save(filePath);
+            bitmap.Save(filePath, ImageFormat.Png);
         }
     }
 }
